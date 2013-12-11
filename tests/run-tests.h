81158fd5ef45310fc08a4c8cee29c864cc30de19
Philip Chimento <philip@endlessm.com>
2013-12-10 21:41:43 -0800

    Added Event Recorder API.
    This is the beginning of the metrics library, with framework for
    documentation, introspection, enable/disable, and unit tests.
    
    Gettext is not needed so far, because this library will contain no
    user-facing functionality.
    
    This is a combination of many commits.
    
    (Philip, Devin, Cosimo & Kurt)

---
+++ tests/run-tests.h
@@ -0,0 +1,10 @@
+/* Copyright 2013 Endless Mobile, Inc. */
+
+#ifndef RUN_TESTS_H
+#define RUN_TESTS_H
+
+/* Each module adds its own test cases: */
+
+void add_event_recorder_tests      (void);
+
+#endif /* RUN_TESTS_H */
